5dfb2dc4b1daffbba2b0dcaea13102f4acb68cfa
raulfragueiro <raulfragueiro@gmail.com>
2020-02-18 11:26:55 +0100

    Diseño pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,5 +121,481 @@
         </div>
         </nav>
         <!-- Barra de Navegación FIN -->
+
+        <!--Carrusel-->
+            <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+
+        <!--Indicadores-->
+        <ol class="carousel-indicators">
+        <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+        </ol>
+        <!--/.Indicadores-->
+
+        <!--Slides-->
+        <div class="carousel-inner" role="listbox">
+
+        <!--Primer slide-->
+        <div class="carousel-item active">
+            <div class="view " style="background-image: url('img/images/foto4.jpeg'); background-repeat: no-repeat; background-size: cover;">
+
+            <!-- Mascara-->
+            <div class="mask rgba-black-light">
+
+            </div>
+            <!-- Mascara-->
+
+            </div>
+        </div>
+        <!--/Primer slide-->
+
+        <!--Segundo slide-->
+        <div class="carousel-item">
+            <div class="view" style="background-image: url('img/images/foto2.jpeg'); background-repeat: no-repeat; background-size: cover;">
+
+            <!-- Mascara-->
+            <div class="mask rgba-black-light">
+
+            </div>
+            <!-- Mask & flexbox options-->
+
+            </div>
+        </div>
+        <!--/Segundo slide-->
+
+        <!--Tercer slide-->
+        <div class="carousel-item">
+            <div class="view" style="background-image: url('img/images/foto5.jpeg'); background-repeat: no-repeat; background-size: cover;">
+
+            <!-- Mascara-->
+            <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+            </div>
+            <!-- Mascara-->
+
+            </div>
+        </div>
+        <!--/Tercer slide-->
+
+        </div>
+        <!--/.Slides-->
+
+        <!--Controles-->
+        <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+        </a>
+        <!--/.controles-->
+
+        </div>
+        <!--/.Carrusel-->
+
+        </header>
+
+        <!--Main layout-->
+        <main>
+            <div class="container">
+
+            <!--Section: Proximo Partido-->
+            <section class="mt-5 wow fadeIn">
+
+                <!--Grid fila-->
+                <div class="row">
+
+                <!--Grid columna-->
+                <div class="col-md-6 mb-4">
+
+                    <img src="img/images/JOR17vsMargolariPArtido.png" class="img-fluid z-depth-1-half"
+                    alt="">
+
+                </div>
+                <!--Grid columna-->
+
+                <!--Grid columna-->
+                <div class="col-md-6 mb-4">
+
+                    <!-- Main -->
+                    <h3 class="h3 mb-3">Próximo Partido</h3>
+                    
+                    <p>Xornada: 17</p>
+                    <p>Rival: Margolari F.S. (1º)</p>
+                    <p>Fecha: 15/02/2020</p>
+                    <p>Hora: 17:30</p>
+                    <p>Lugar: Pabellón de Mondariz</p>
+                    <!-- CTA -->
+                    <a target="_blank" href="/img/images/JOR17vsMargolari.png" class="btn btn-grey btn-md">Convocados
+                    </a>
+                    <a target="_blank" href="https://goo.gl/maps/Mc7KBJpQh5DBaHii6" class="btn btn-grey btn-md">Ruta al Campo
+                    </a>
+
+                </div>
+                <!--Grid columna-->
+
+                </div>
+                <!--Grid fila-->
+
+            </section>
+            <!--Section: Proximo Partido-->
+
+            <!--Section: Resultados-->
+            <section class="mt-5 wow fadeIn">
+
+                <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                    <img src="img/images/JOR16.png" class="img-fluid z-depth-1-half">
+                    </div>
+                    <div class="col-sm">
+                    <img src="img/images/JOR15.png" class="img-fluid z-depth-1-half">     
+                    </div>
+                    <div class="col-sm">
+                    <img src="img/images/JOR14.png" class="img-fluid z-depth-1-half">     
+                    </div>
+                </div>
+                </div>
+
+            </section>
+            <!--Section: Resultados-->
+
+            <hr class="my-5">
+
+            <!--Section: Products v.3-->
+            <section class="text-center mb-4">
+
+                <!--Grid row-->
+                <div class="row wow fadeIn">
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4">
+
+                    <!--Card-->
+                    <div class="card">
+
+                    <!--Card image-->
+                    <div class="view overlay">
+                        <img src="img/images/CAMISETA_CALENTAR_GULANS_DEL.png" class="card-img-top"
+                        alt="">
+                        <a>
+                        <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <!--Card image-->
+
+                    <!--Card content-->
+                    <div class="card-body text-center">
+                        <!--Category & Title-->
+                        <h6>
+                        <strong>
+                            <a href="" class="dark-grey-text">Camiseta Calentamiento 2019/2020</a>
+                        </strong>
+                        </h6>
+
+                        <h4 class="font-weight-bold blue-text">
+                        <strong>25,99€</strong>
+                        </h4>
+
+                    </div>
+                    <!--Card content-->
+
+                    </div>
+                    <!--Card-->
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4">
+
+                    <!--Card-->
+                    <div class="card">
+
+                    <!--Card image-->
+                    <div class="view overlay">
+                        <img src="img/images/CAMISETA_GULANSFS_DEL.jpg" class="card-img-top" alt="">
+                        <a>
+                        <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <!--Card image-->
+
+                    <!--Card content-->
+                    <div class="card-body text-center">
+                        <!--Category & Title-->
+                        <h5>
+                        <strong>
+                            <a href="" class="dark-grey-text">Camiseta Principal 2019/2020</a>
+                        </strong>
+                        </h5>
+
+                        <h4 class="font-weight-bold blue-text">
+                        <strong>34,99€</strong>
+                        </h4>
+
+                    </div>
+                    <!--Card content-->
+
+                    </div>
+                    <!--Card-->
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4">
+
+                    <!--Card-->
+                    <div class="card">
+
+                    <!--Card image-->
+                    <div class="view overlay">
+                        <img src="/img/images/SUDADERA.jpg" class="card-img-top"
+                        alt="">
+                        <a>
+                        <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <!--Card image-->
+
+                    <!--Card content-->
+                    <div class="card-body text-center">
+                        <!--Category & Title-->
+                        <h5>
+                        <strong>
+                            <a href="" class="dark-grey-text">Sudadera Calentamiento
+                            </a>
+                        </strong>
+                        </h5>
+
+                        <h4 class="font-weight-bold blue-text">
+                        <strong>49,99€</strong>
+                        </h4>
+
+                    </div>
+                    <!--Card content-->
+
+                    </div>
+                    <!--Card-->
+
+                </div>
+                <!--Grid column-->
+
+                <!--Fourth column-->
+                <div class="col-lg-3 col-md-6 mb-4">
+
+                    <!--Card-->
+                    <div class="card">
+
+                    <!--Card image-->
+                    <div class="view overlay">
+                        <img src="/img/images/POLO_GULANSFS.png" class="card-img-top"
+                        alt="">
+                        <a>
+                        <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <!--Card image-->
+
+                    <!--Card content-->
+                    <div class="card-body text-center">
+                        <!--Category & Title-->
+                        <h5>
+                        <strong>
+                            <a href="" class="dark-grey-text">Polo Oficial</a>
+                        </strong>
+                        </h5>
+
+                        <h4 class="font-weight-bold blue-text">
+                        <strong>39,99$</strong>
+                        </h4>
+
+                    </div>
+                    <!--Card content-->
+
+                    </div>
+                    <!--Card-->
+
+                </div>
+                <!--Fourth column-->
+
+                </div>
+                <!--Grid row-->
+
+
+            </section>
+            <!--Section: Products v.3-->
+            <hr class="my-5">
+            <!--Section: Stats & Clasificacion-->
+            <section>
+
+                <!--Grid fila-->
+                <div class="row wow fadeIn">
+
+                <!--Grid columna-->
+                <div class="col-lg-6 col-md-12 px-4">
+
+                    <!--Primera fila-->
+                    <div class="row text-center">
+                    <div class="col-10">
+                        <table id="dtBasicExample" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                        <thead class="green">
+                            <tr>
+                            <th class="th-sm">Part.
+                            </th>
+                            <th class="th-sm">Vic.
+                            </th>
+                            <th class="th-sm">Emp.
+                            </th>
+                            <th class="th-sm">Der.
+                            </th>
+                            <th class="th-sm">Goles
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>15</td>
+                            <td>5</td>
+                            <td>2</td>
+                            <td>8</td>
+                            <td>40</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
+                    </div>
+                    <!--/Primera fila-->
+
+
+                    <!--Segunda fila-->
+                    <div class="row">
+                    <div class="col-10">
+                        <h5 class="feature-title text-center">Goleadores</h5>
+                        <table id="dtBasicExample" class="table table-striped table-bordered text-center" cellspacing="0" width="100%">
+                        <thead class="green">
+                            <tr>
+                            <th class="th-sm">
+                            </th>
+                            <th class="th-sm">Nombre
+                            </th>
+                            <th class="th-sm">Goles.
+                            </th>
+                            <th class="th-sm">Partidos
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td><img src="/img/images/jugador.png" height="20"></td>
+                            <td>David Fernandez</td>
+                            <td>8</td>
+                            <td>8</td>
+                            </tr>
+                            <tr>
+                            <td><img src="/img/images/jugador.png" height="20"></td>
+                            <td>David Fernandez</td>
+                            <td>8</td>
+                            <td>8</td>
+                            </tr>
+                            <tr>
+                            <td><img src="/img/images/jugador.png" height="20"></td>
+                            <td>David Fernandez</td>
+                            <td>8</td>
+                            <td>8</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
+                    </div>
+                    <!--/Segunda fila-->
+
+                    <div style="height:30px"></div>
+                </div>
+                <!--/Grid columna-->
+
+                <!--Grid columna-->
+                <div class="col-lg-6 col-md-12">
+                    <div style="font-size: 1em;line-height: 1em;background: #000;color: #fff;margin: 0 auto;padding: 0.3em 0;width:90%; text-align:center">
+                    <a target="_blank" style="color:#FFF;text-decoration:none" 
+                    href="https://www.siguetuliga.com/liga/galicia-ligas-locales-pontevedra-liga-metropolitana-fs-condado-honor/clasificacion?utm_source=widget2&utm_medium=webs%2Bo%2Bblogs&utm_campaign=Widgets%2Bresultados">
+                    Clasificacion Liga Metropolitana FS Condado Honor</a>
+                </div>
+                <script type="text/javascript">widthIcon = "14";heightIcon = "14";</script>
+                <script language="javascript" src="https://www.siguetuliga.com/widget/clasificacion2.php?id=3899" charset="iso-8859-1"></script>
+                </div>
+                <!--/Grid columna-->
+
+                </div>
+                <!--/Grid fila-->
+
+            </section>
+            <!--Section: Stats & Clasificacion-->
+
+            </div>
+        </main>
+        <!--Main layout-->
+
+        <!--Footer-->
+        <!-- Footer -->
+        <footer class="page-footer font-small stylish-color-dark pt-4">
+        <!-- Call to action -->
+        <ul class="list-unstyled list-inline text-center py-2">
+            <li class="list-inline-item">
+            <h5 class="mb-1">Rexistrate</h5>
+            </li>
+            <li class="list-inline-item">
+            <a href="#!" class="btn btn-danger btn-rounded">Iniciar Sesión</a>
+            </li>
+        </ul>
+        <!-- Call to action -->
+
+        <hr>
+
+        <!-- Social buttons -->
+        <ul class="list-unstyled list-inline text-center">
+            <li class="list-inline-item">
+            <a class="btn-floating btn-fb mx-1">
+                <i class="fab fa-facebook-f"> </i>
+            </a>
+            </li>
+            <li class="list-inline-item">
+            <a class="btn-floating btn-tw mx-1">
+                <i class="fab fa-twitter"> </i>
+            </a>
+            </li>
+            <li class="list-inline-item">
+            <a class="btn-floating btn-gplus mx-1">
+                <i class="fab fa-google-plus-g"> </i>
+            </a>
+            </li>
+        </ul>
+        <!-- Social buttons -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="index.html"> Gulans F.S.</a>
+        </div>
+        <!-- Copyright -->
+
+        </footer>
+        <!-- Footer -->
+        <!-- SCRIPTS -->
+        <!-- JQuery -->
+        <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="js/popper.min.js"></script>
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="js/mdb.min.js"></script>
+        <!-- Initializations -->
+        <script type="text/javascript">
+            // Animations initialization
+            new WOW().init();
+
+        </script>
     </body>
 </html>
