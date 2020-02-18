9a7982111e1ed7f0135748144f1f24433a6d1a90
raulfragueiro <raulfragueiro@gmail.com>
2020-02-18 11:17:43 +0100

    Barra de Navegación

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,122 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+        <title>Inicio | Guláns Fútbol Sala</title>
+        <link rel="icon" type="image/vnd.microsoft.icon" href="/img/logo.ico">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="css/mdb.min.css" rel="stylesheet">
+        <!-- Your custom styles (optional) -->
+        <link href="css/style.min.css" rel="stylesheet">
+        <style type="text/css">
+            html,
+            body,
+            header,
+            .carousel {
+                height: 60vh;
             }
 
-            .full-height {
-                height: 100vh;
-            }
+            @media (max-width: 740px) {
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
+                html,
+                body,
+                header,
+                .carousel {
+                    height: 100vh;
+                }
             }
+            @media (min-width: 800px) and (max-width: 850px) {
 
-            .position-ref {
-                position: relative;
+                html,
+                body,
+                header,
+                .carousel {
+                    height: 100vh;
+                }
             }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            @media (min-width: 800px) and (max-width: 850px) {
+                .navbar:not(.top-nav-collapse) {
+                    background: #929FBA !important;
+                }
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <header>
+        <!-- Barra de Navegación INICIO-->
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+        <div class="container">
+            <!-- Logo -->
+            <a class="navbar-brand" href="index.html" target="_blank">
+            <img src="img/images/logo.png" height="50">
+            </a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <!-- Collapse -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <!-- Links -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <!-- Barra Izquierda -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                <a class="nav-link" href="#">Inicio
+                    <span class="sr-only">(current)</span>
+                </a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">Club</a>
+                <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Datos do Club</a>
+                    <a class="dropdown-item" href="#">Contacto</a>
                 </div>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">Primer Equipo</a>
+                <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Plantilla</a>
+                    <a class="dropdown-item" href="#">Estadísticas</a>
+                    <a class="dropdown-item" href="#">Liga Metropolitana</a>
+                    <a class="dropdown-item" href="#">Agenda</a>
+                </div>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="" target="_blank">Tenda</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="" target="_blank">Patrocinadores</a>
+                </li>
+            </ul>
+
+            <!-- Barra Derecha -->
+            <ul class="navbar-nav nav-flex-icons">
+                <li class="nav-item">
+                <a href="https://www.facebook.com/gulansfs" class="nav-link" target="_blank">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href="https://twitter.com/gulansfs" class="nav-link" target="_blank">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href="https://www.instagram.com/gulansfs" class="nav-link" target="_blank">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                </li>
+            </ul>
             </div>
         </div>
+        </nav>
+        <!-- Barra de Navegación FIN -->
     </body>
 </html>
